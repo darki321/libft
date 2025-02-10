ea962aa84b92d59927ae540db412bbfcc2845c1f
darki321 <mohapouge@gmail.com>
2025-02-10 12:38:21 +0100

    first push of libft

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molapoug <molapoug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/09 15:13:35 by tchevrie          #+#    #+#             */
+/*   Updated: 2022/11/08 11:03:58 by tchevrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	result;
+	int	sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] >= 9 && str[i] <= 13)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molapoug <molapoug@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/09 15:13:35 by tchevrie          #+#    #+#             */
+/*   Updated: 2022/11/08 11:03:58 by tchevrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+/* fonctions prototypés avec des int */
+int		ft_strlen(char *str);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_atoi(const char *str);
+
+/* fonctions prototypés avec des char*/
+
+/* fonctions prototypés avec des void */
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putendl(const char *s);
+
+#endif
