2892d8a157b234595ba7a86ab7b5bafd146cf6e2
darki321 <mohapouge@gmail.com>
2025-02-12 12:33:23 +0100

    norminette update

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: molapoug <molapoug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 12:20:01 by molapoug          #+#    #+#             */
-/*   Updated: 2025/02/12 12:30:23 by molapoug         ###   ########.fr       */
+/*   Updated: 2025/02/12 12:32:50 by molapoug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,7 @@
 
 int	ft_isalnum(int c)
 {
-	if ((c == ft_isalpha(c)) || (c ==ft_isdigit(c)))
+	if ((c == ft_isalpha(c)) || (c == ft_isdigit(c)))
 		return (1);
 	return (0);
-}
-
-#include <stdio.h>
-#include <ctype.h>
-int	main()
-{
-	char	str[] = "test";
-	printf("%d\n", ft_isalnum(str[4]));
-	printf("%d\n", isalnum(str[4]));
 }
